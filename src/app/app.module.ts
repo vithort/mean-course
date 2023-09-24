0bb7b506486607a385465b3746bb648a6b6eee88
Vithor Carvalho <vithort@gmail.com>
2023-09-24 17:32:46 +0100

    21. Adding a Toolbar
    > ng g c header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
@@ -5,17 +6,18 @@ import { NgModule } from "@angular/core";
 import {
   MatButtonModule,
   MatCardModule,
-  MatFormFieldModule,
   MatInputModule,
+  MatToolbarModule,
 } from "@angular/material";
 
 import { AppComponent } from "./app.component";
 
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
+  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
@@ -23,8 +25,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     // Angular Material
     MatButtonModule,
     MatCardModule,
-    MatFormFieldModule,
     MatInputModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
