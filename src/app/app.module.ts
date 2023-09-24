3140f9462c950a1e8571008dec029e970750176d
Vithor Carvalho <vithort@gmail.com>
2023-09-24 17:26:47 +0100

    20. Installing Angular Material
    > ng add @angular/material@8
    
    or
    
    > npm i --save @angular/material@8
    
    if need exact version or downgrade:
    
    > npm i --save @angular/material@8 --save-exact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+} from "@angular/material";
+
 import { AppComponent } from "./app.component";
 
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    // Angular Material
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
