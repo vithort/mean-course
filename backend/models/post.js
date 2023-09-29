1c333a026bfed2b5b73f148f61046574d1fa2960
Vithor Carvalho <vithort@gmail.com>
2023-09-29 17:34:17 +0100

    52. Creating a POST Instance

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const postSchema = mongoose.Schema({
-  title: { tipe: String, require: true },
-  content: { tipe: String, require: true },
+  title: { type: String, require: true },
+  content: { type: String, require: true },
 });
 
 module.exports = mongoose.model("Post", postSchema);
