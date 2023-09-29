21738afdc19cb5baac6f56b4adab9e984fe542b7
Vithor Carvalho <vithort@gmail.com>
2023-09-29 09:48:57 +0100

    25. Creating a Post Model

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,18 +1,20 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 
+import { Post } from "../post.model";
+
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
   styleUrls: ["./post-create.component.css"],
 })
 export class PostCreateComponent {
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
   enteredContent = "";
   enteredTitle = "";
 
   onAddPost(): void {
-    const post = { title: this.enteredTitle, content: this.enteredContent };
+    const post: Post = { title: this.enteredTitle, content: this.enteredContent };
     this.postCreated.emit(post);
   }
 }
