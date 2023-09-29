63c6b8391dd45191221c807d1fe5a92cb090f709
Vithor Carvalho <vithort@gmail.com>
2023-09-29 22:54:40 +0100

    58. Updating the Frontend after Deleting Posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -49,9 +49,11 @@ export class PostsService {
 
   deletePost(postId: string): void {
     this.http
-      .delete(`http://localhost:3000/api/posts/${postId}`)
+      .delete("http://localhost:3000/api/posts/" + postId)
       .subscribe(() => {
-        console.log("Deleted!");
+        const updatedPosts = this.posts.filter((post) => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
       });
   }
 }
