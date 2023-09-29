789c1f19e1cf50cbb4d3dacb386667287210e70e
Vithor Carvalho <vithort@gmail.com>
2023-09-29 21:46:45 +0100

    54. Storing Data in a Database

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content,
   });
-  console.log(post);
+  post.save();
   res.status(201).json({ message: "Post added successfully!" });
 });
 
