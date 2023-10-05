d86099be40e65e23f7f9ddfd6aadf1c7767e8b62
Vithor Carvalho <vithort@gmail.com>
2023-10-05 10:25:30 +0100

    96. Handling User Input

--- src/app/auth/login/login/login.component.ts
+++ src/app/auth/login/login/login.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   isLoading = false;
 
-  constructor() {}
-
-  ngOnInit() {}
+  onLogin(form: NgForm): void {
+    console.log(form.value);
+  }
 }
