ebd63d42fc08d7a3462d41eb66a4d2fd4c4abb19
Vithor Carvalho <vithort@gmail.com>
2023-10-05 13:14:51 +0100

    108. Connecting the Logout Button to the Authentication Status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -45,4 +45,10 @@ export class AuthService {
         }
       });
   }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -26,4 +26,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.authListenerSubs.unsubscribe();
   }
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
